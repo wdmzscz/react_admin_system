d95aec3d145d3db0ddaedeed07fc0fc887c003bf
cjzcheng@yahoo.com <cjzcheng@yahoo.com>
2020-11-21 23:16:05 -0500

    build router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
-import 'antd/dist/antd.less';
-import {Button} from 'antd'
+import {Route, Switch} from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 export default class App extends Component{
     render(){
        return(
-          <div className="App">
-            <Button type="primary">Button</Button>
+          <div>
+            <Switch>
+               <Route path='/login' component={Login}/>
+               <Route path='/admin' component={Admin}/>
+            </Switch>
           </div>
        )
     }
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/login.less'
+
+export default class Login extends React.Component{
+    render(){
+       return(
+          <div>
+            <header>
+                <img src="./img/bg.png" alt=""/>
+                <h1>Product Managment System</h1>
+            </header>
+          </div>
+       )
+    }
+}
