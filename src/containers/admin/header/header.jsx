4f408c868b4d96f8118447f59eff91bef505e0c4
cjzcheng@yahoo.com <cjzcheng@yahoo.com>
2020-12-14 11:29:39 -0500

    set full screen

--- src/containers/admin/header/header.jsx
+++ src/containers/admin/header/header.jsx
@@ -2,13 +2,34 @@ import React, {Component} from 'react';
 import '../css/header.less';
 import {FullscreenOutlined} from '@ant-design/icons';
 import {Button} from 'antd';
+import screenfull from 'screenfull'
 
 export default class Header extends Component{
+
+   state={
+      isFull:false
+   }
+
+   fullScreen=()=>{
+      screenfull.toggle()
+   }
+
+   componentDidMount(){
+      screenfull.on('change',()=>{
+         let isFull = !this.state.isFull;
+         this.setState({isFull})
+      })
+   }
+
     render(){
        return(
          <header className='header'>
             <div className='header-top'>
-               <Button size='small'><FullscreenOutlined /></Button>
+               <Button size='small' onClick={this.fullScreen}>
+               {this.state.isFull
+                  ?<FullscreenOutlined />
+                  :<FullscreenExitOutlined />}
+               </Button>
                <span className='userName'>welcome</span>
                <Button type='link'>Log out</Button>
             </div>
@@ -19,7 +40,6 @@ export default class Header extends Component{
                <div className='header-bottom-right'>
                   2020-12-13
                </div>
-               <img src='https://weather-ydn-yql.media.yahoo.com/forecastrss' alt='weather'/>
             </div>
          </header>
        )
