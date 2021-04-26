c296f12fcafdea81e40997aa2b411946300e8368
cjzcheng@yahoo.com <cjzcheng@yahoo.com>
2021-04-25 23:52:56 -0400

    finished the get Category and build table

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,4 @@
 //该文件为项目配置文件，save 
 export const BASE_URL = '' //
+
+export const PAGE_SIZE = '1'
--- src/containers/category/category.jsx
+++ src/containers/category/category.jsx
@@ -1,9 +1,59 @@
 import React, {Component} from 'react'
+import { Card, Button, message, Table } from 'antd';
+import {reqCategoryList} from '../../api';
+import {PAGE_SIZE} from '../../config';
 
 export default class Category extends Component{
+
+   state={
+      Category:[]
+   }
+
+   getCategoryList= async ()=>{
+      let result = await reqCategoryList();
+      let {status, msg, data } = result;
+               console.log('data',result)
+      if(status === 200){
+         this.setState({Category:data.data});
+
+      }else{
+         message.error(msg,1);
+      }
+   }
+
+   componentDidMount(){
+      this.getCategoryList();
+   }
+
     render(){
-       return(
-          <div>Category</div>
-       )
+
+       const dataSource = this.state.Category;
+
+         const columns = [
+            {
+               title: 'Category',
+               dataIndex: 'name',
+               key: 'name',
+            },
+            {
+               title: 'Action',
+               dataIndex: 'age',
+               key: 'age',
+               render:()=>{return(<Button type='primary'>Click</Button>)},
+               width:'25%',
+               align:'center'
+            }
+         ];
+         return(
+            <Card title="Default size card" extra={<Button type = 'primary'>Add</Button>} >
+               <Table 
+                  dataSource={dataSource} 
+                  columns={columns} 
+                  bordered 
+                  rowKey='_id'
+                  pagination={{pageSize:PAGE_SIZE}}
+               />
+            </Card>
+         )
     }
 }
